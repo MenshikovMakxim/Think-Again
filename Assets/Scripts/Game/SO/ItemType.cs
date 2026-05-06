55f0b88c36ce64425b2da565a5e87fa5c6a61d05
Riabokon Stas Igorevich <131764071+layyyzi@users.noreply.github.com>
2026-05-06 16:45:55 +0300

    Update Level Test and Main scene

--- Assets/Scripts/Game/SO/ItemType.cs
+++ Assets/Scripts/Game/SO/ItemType.cs
@@ -1,13 +1,24 @@
 ﻿
 namespace Game.SO
 {
-  public enum ItemType
-  {
-      None,
-      Axe,
-      Wood,
-      Plank
-  }  
+    public enum ItemType
+    {
+        None,
+        Axe,
+        Wood,
+        Plank,
+        Banana,
+        Monkey,
+        Monkey_banana,
+        Funnel,
+        Funnel_no_water,
+        Palm,
+        Palm_banana,
+        Question,
+        Stick,
+        Stick_hook,
+        Vase
+    }
 }
 
 // додавати нові обєкти потрібно тільки внизу списку, інакше вся система зібється
