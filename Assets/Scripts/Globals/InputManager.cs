f9b301d9ffae19d390f4840eb253eccb689f99b7
MENSHYKOV MAKSYM <menshikov5254@gmail.com>
2026-05-11 20:26:12 +0300

    виправлення помилки input

--- Assets/Scripts/Globals/InputManager.cs
+++ Assets/Scripts/Globals/InputManager.cs
@@ -1,97 +1,3 @@
-// using UnityEngine;
-// using UnityEngine.EventSystems;
-// using UnityEngine.InputSystem;
-// using Game.Interfaces;
-//
-// public class InputManager : MonoBehaviour
-// {
-//     [SerializeField] private GameObject gameCursor;
-//     private IDraggable _currentDraggedObject;
-//     private UICursor _uiCursor;
-//
-//     private void Awake()
-//     {
-//         _uiCursor = gameCursor.GetComponent<UICursor>();
-//     }
-//
-//     void Update()
-//     {
-//         Vector2 screenPosition = Vector2.zero;
-//         bool isPressDown = false;
-//         bool isPressing = false;
-//         bool isPressUp = false;
-//         
-//         if (Touchscreen.current != null && Touchscreen.current.primaryTouch.press.isPressed)
-//         {
-//             var touch = Touchscreen.current.primaryTouch;
-//             screenPosition = touch.position.ReadValue();
-//             isPressDown = touch.press.wasPressedThisFrame;
-//             isPressing = touch.press.isPressed;
-//             isPressUp = touch.press.wasReleasedThisFrame;
-//         }
-//         else if (Mouse.current != null)
-//         {
-//             screenPosition = Mouse.current.position.ReadValue();
-//             isPressDown = Mouse.current.leftButton.wasPressedThisFrame;
-//             isPressing = Mouse.current.leftButton.isPressed;
-//             isPressUp = Mouse.current.leftButton.wasReleasedThisFrame;
-//         }
-//         
-//         if (isPressDown && EventSystem.current.IsPointerOverGameObject()) return;
-//         
-//         Vector2 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
-//         Vector3 tempScreenPos = new Vector3(screenPosition.x, screenPosition.y, 10f); 
-//         worldPosition = Camera.main.ScreenToWorldPoint(tempScreenPos);
-//         
-//         if (isPressDown)
-//         {
-//             RaycastHit2D hit = Physics2D.Raycast(worldPosition, Vector2.zero);
-//             if (hit.collider is not null)
-//             {
-//                 IDraggable draggable = hit.collider.GetComponent<IDraggable>();
-//                 if (draggable != null)
-//                 {
-//                     _currentDraggedObject = draggable;
-//                     _currentDraggedObject.OnBeginDrag(worldPosition);
-//                     _uiCursor.ActiveHolding(true);
-//                     
-//                 }
-//                 else
-//                 {
-//                     IClickable[] clickables = hit.collider.GetComponents<IClickable>();
-//
-//                     foreach (var clickable in clickables)
-//                     {
-//                         clickable.OnClick();
-//                     }
-//                 }
-//             }
-//         }
-//         
-//         if (isPressing && _currentDraggedObject != null)
-//         {
-//             if (_currentDraggedObject as MonoBehaviour == null)
-//             {
-//                 _currentDraggedObject = null;
-//             }
-//             else
-//             {
-//                 _currentDraggedObject.OnDrag(worldPosition);
-//             }
-//         }
-//         
-//         if (isPressUp && _currentDraggedObject != null)
-//         {
-//             if (_currentDraggedObject as MonoBehaviour != null)
-//             {
-//                 _currentDraggedObject.OnEndDrag();
-//                 _uiCursor.ActiveHolding(false);
-//             }
-//             
-//             _currentDraggedObject = null; 
-//         }
-//     }
-// }
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
@@ -142,22 +48,42 @@ public class InputManager : MonoBehaviour
         
         if (isPressDown)
         {
-            RaycastHit2D hit = Physics2D.Raycast(worldPosition, Vector2.zero);
-            if (hit.collider is not null)
+            Collider2D[] hits = Physics2D.OverlapPointAll(worldPosition);
+            
+            if (hits.Length > 0)
             {
-                IDraggable draggable = hit.collider.GetComponent<IDraggable>();
-                if (draggable != null)
+                Collider2D topCollider = null;
+                int highestOrder = int.MinValue;
+            
+                foreach (var hit in hits)
                 {
-                    _currentDraggedObject = draggable;
-                    _currentDraggedObject.OnBeginDrag(worldPosition);
-                    _uiCursor.ActiveHolding(true);
-                }
-                else
-                {
-                    IClickable[] clickables = hit.collider.GetComponents<IClickable>();
-                    foreach (var clickable in clickables)
+                    SpriteRenderer sr = hit.GetComponent<SpriteRenderer>();
+            
+                    if (sr != null && sr.sortingOrder > highestOrder)
                     {
-                        clickable.OnClick();
+                        highestOrder = sr.sortingOrder;
+                        topCollider = hit;
+                    }
+                }
+            
+                if (topCollider != null)
+                {
+                    IDraggable draggable = topCollider.GetComponent<IDraggable>();
+            
+                    if (draggable != null)
+                    {
+                        _currentDraggedObject = draggable;
+                        _currentDraggedObject.OnBeginDrag(worldPosition);
+                        _uiCursor.ActiveHolding(true);
+                    }
+                    else
+                    {
+                        IClickable[] clickables = topCollider.GetComponents<IClickable>();
+            
+                        foreach (var clickable in clickables)
+                        {
+                            clickable.OnClick();
+                        }
                     }
                 }
             }
